bd35da37267a9a7a6ba29d8ede0fb42f80afcb7a
Thiago Santos <thiisantos00@gmail.com>
2024-01-31 03:27:54 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import Header from "../_components/header";
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
-      <Header/>
+      <Header />
+
+    <div className="px-5 pt-3">
+    <h2 className="text-xl font-bold">Ola, Thiago!</h2>
+      <p className="capitalize text-sm">
+        {format(new Date(), "EEEE',' dd 'de' MMMM",{
+          locale: ptBR
+        })}
+      </p>
+    </div>
     </div>
   );
 }
