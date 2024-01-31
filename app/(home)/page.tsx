82192ed5783b571342c0ed9fe5e02c202169eaca
Thiago Santos <thiisantos00@gmail.com>
2024-01-31 03:44:47 -0300

    feat: add initial search components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,20 +2,25 @@ import Image from "next/image";
 import Header from "../_components/header";
 import { format } from "date-fns"
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
 
-    <div className="px-5 pt-3">
-    <h2 className="text-xl font-bold">Ola, Thiago!</h2>
-      <p className="capitalize text-sm">
-        {format(new Date(), "EEEE',' dd 'de' MMMM",{
-          locale: ptBR
-        })}
-      </p>
-    </div>
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Ola, Thiago!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR
+          })}
+        </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
     </div>
   );
 }
